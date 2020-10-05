d6b986aeb876f4f207570fa1452aee9f5b4e9177
Oleksandr Poliarush <oleksandr@Oleksandrs-MacBook-Pro.local>
2020-10-05 10:29:36 +0300

    refactor env package

--- cmd/go-materials/main.go
+++ cmd/go-materials/main.go
@@ -3,63 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-
-	"github.com/oleksandr-pol/simple-go-service/pkg/storage"
-
-	"github.com/oleksandr-pol/simple-go-service/pkg/logger"
 
 	"github.com/oleksandr-pol/simple-go-service/internal/env"
-	"github.com/oleksandr-pol/simple-go-service/internal/models"
 	"github.com/oleksandr-pol/simple-go-service/internal/routes"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	const (
-		hostname     = "localhost"
-		host_port    = 5432
-		username     = "oleksandr"
-		password     = "test"
-		databasename = "mentorship"
-	)
-
-	pg_con_string := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		hostname, host_port, username, password, databasename)
-	fmt.Println(pg_con_string)
-	db, err := models.NewDB(pg_con_string)
-
+	server := env.NewServer()
+	err := routes.RegisterRoutes(server)
 	if err != nil {
-		log.Panic(err)
+		server.Logger.ServerError(err.Error())
 	}
 
-	standartLogger := logger.NewLogger(os.Stdout)
-	memoryStorage := storage.NewStorage()
-
-	server := &env.Server{
-		Router:  mux.NewRouter(),
-		Db:      db,
-		Logger:  standartLogger,
-		Storage: memoryStorage,
-	}
-
-	setUpServer(server)
-}
-
-func setUpServer(s *env.Server) {
 	var port int
 	flag.IntVar(&port, "p", 8000, "specify port to use.  defaults to 8000")
 	flag.Parse()
 
-	err := routes.RegisterRoutes(s)
-	if err != nil {
-		s.Logger.ServerError(err.Error())
-	}
-
-	log.Println(fmt.Sprintf("Listing for requests at http://localhost:%d", port))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), s.Router))
+	server.Logger.Info(fmt.Sprintf("Listing for requests at http://localhost:%d", port))
+	server.Logger.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server.Router))
 }
--- internal/env/server.go
+++ internal/env/server.go
@@ -1,6 +1,10 @@
 package env
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gorilla/mux"
 	"github.com/oleksandr-pol/simple-go-service/internal/models"
 	"github.com/oleksandr-pol/simple-go-service/pkg/logger"
@@ -12,4 +16,34 @@ type Server struct {
 	Db      models.DataStore
 	Logger  *logger.StandardLogger
 	Storage *storage.Storage
+}
+
+func NewServer() *Server {
+	const (
+		hostname     = "localhost"
+		host_port    = 5432
+		username     = "oleksandr"
+		password     = "test"
+		databasename = "mentorship"
+	)
+
+	pg_con_string := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		hostname, host_port, username, password, databasename)
+	fmt.Println(pg_con_string)
+	db, err := models.NewDB(pg_con_string)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	standartLogger := logger.NewLogger(os.Stdout)
+	memoryStorage := storage.NewStorage()
+
+	return &Server{
+		Router:  mux.NewRouter(),
+		Db:      db,
+		Logger:  standartLogger,
+		Storage: memoryStorage,
+	}
 }
